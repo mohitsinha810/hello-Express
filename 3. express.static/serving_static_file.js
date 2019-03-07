2ae69f055aaf7054e60cdb8ef746349399e42dc2
Mohit <mohitsinha810@gmail.com>
2019-03-07 21:55:35 +0545

    Added static file serving code

--- 3. express.static/serving_static_file.js
+++ 3. express.static/serving_static_file.js
@@ -4,11 +4,15 @@ const path = require("path");
 
 let app = express();
 
+let publicPath = path.resolve(__dirname, "public");
+app.use(express.static(publicPath));
+//Try this url: loclhost:3000/image.jpg
+
 app.use(logger('short'));
 
 app.use(function(req,res){
-	res.json({hello : 'world'});
-	//or res.send({hello : 'world'});
+	res.writeHead(200, {"Content-Type" : "text/plain"});
+	res.end("Founded no static file");
 });
 
 app.listen(3000,()=>{console.log("App started at localhost:3000")});
