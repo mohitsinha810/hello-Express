78ccb2e32b3b8843eae7cb017eb944745c5a040a
Mohit <mohitsinha810@gmail.com>
2019-03-18 20:07:08 +0545

    Added some templates examples

---
+++ 7. Templates/ejs.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+
+let app = express();
+
+app.locals.name = "Test App";
+app.locals.lists = ["Coffee","Tea"];
+
+app.set("view engine","ejs");
+app.set("views",path.resolve(__dirname,"views"));
+
+app.use(logger("dev"));
+
+app.get("/",function(req,res){
+	res.render("index");
+});
+
+app.get("/about",function(req,res){
+	res.render("about");
+});
+
+app.listen(3000,() => console.log("App started"));
